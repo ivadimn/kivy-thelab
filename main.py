4536bf989e9f58a24ef1a1cd002804b3a77a4eea
vadim <ivadimn@mail.ru>
2022-01-16 21:40:50 +0300

    kivy lab

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.lang import Builder
+
+Builder.load_file("TheLab.kv")
 
 class BoxLayoutExample(BoxLayout):
     def __init__(self, **kwargs):
